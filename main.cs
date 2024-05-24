a2b7c64f7d1ac747509650004411cd02800ef9b2
aoyola6 <116612522+aoyola6@users.noreply.github.com>
2024-05-24 03:07:35 +0000

    Unit Test 2 Commit

--- main.cs
+++ main.cs
@@ -2,7 +2,7 @@ using System;
 
 class Program {
   public static void Main (string[] args) {
-    Console.WriteLine ("Lucinda Potter");
+    Console.WriteLine ("Robert Hall");
     Console.WriteLine ("6/24/1992");
     Console.WriteLine ("work phone: 407-555-1234");
     Console.WriteLine("cell phone: 407-555-4567");
