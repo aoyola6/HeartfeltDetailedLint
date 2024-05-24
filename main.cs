6476a70bc0c238578d191969c826f67529a20f3e
aoyola6 <116612522+aoyola6@users.noreply.github.com>
2024-05-24 03:05:08 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    Console.WriteLine ("Lucinda Potter");
+    Console.WriteLine ("6/24/1992");
+    Console.WriteLine ("work phone: 407-555-1234");
+    Console.WriteLine("cell phone: 407-555-4567");
+    
+  }
+}
